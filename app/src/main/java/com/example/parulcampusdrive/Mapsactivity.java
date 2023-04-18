45c6e5bdb04bffea0af8d99f36039f448fe30fa7
mehariyaniraj <210303108016@paruluniversity.ac.in>
2023-04-18 11:58:55 +0530

    map types added

--- app/src/main/java/com/example/parulcampusdrive/Mapsactivity.java
+++ app/src/main/java/com/example/parulcampusdrive/Mapsactivity.java
@@ -4,6 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,6 +19,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class Mapsactivity extends AppCompatActivity implements OnMapReadyCallback {
 GoogleMap gMap;
 FrameLayout map;
+    private GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +33,35 @@ FrameLayout map;
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         this.gMap =googleMap;
-        LatLng mapindia=new LatLng(22.2887,73.3634);
-        this.gMap.addMarker(new MarkerOptions().position(mapindia).title("Marker in india "));
-        this.gMap.moveCamera(CameraUpdateFactory.newLatLng(mapindia));
+        LatLng paruluniversity=new LatLng(22.2887,73.3634);
+        this.gMap.addMarker(new MarkerOptions().position(paruluniversity).title("Marker in india "));
+        this.gMap.moveCamera(CameraUpdateFactory.newLatLng(paruluniversity));
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Change the map type based on the user's selection.
+        switch (item.getItemId()) {
+            case R.id.normal_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                return true;
+            case R.id.hybrid_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                return true;
+            case R.id.satellite_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                return true;
+            case R.id.terrain_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        
     }
 }
